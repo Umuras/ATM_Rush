ff2dcaa996f82ab3f0554a3e9c2e8f710ccb17cc
Ali Umur Kucur <aliumurkucur@gmail.com>
2023-12-24 11:32:56 +0300

    When pass next level score value was zero. This problem fixed.

--- Assets/Scripts/Runtime/Managers/UIManager.cs
+++ Assets/Scripts/Runtime/Managers/UIManager.cs
@@ -34,6 +34,7 @@ public class UIManager : MonoBehaviour
         CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Level, 1);
         CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Shop, 2);
         UISignals.Instance.onSetNewLevelValue?.Invoke(levelValue);
+        UISignals.Instance.onSetMoneyValue?.Invoke((int)ScoreSignals.Instance.onGetMoney?.Invoke());
     }
 
     public void OnPlay()
